3e30c42f16b9e61153b91fa835be0fb14c35b286
fendy <2457037852@qq.com>
2021-12-21 18:38:45 +0800

    aside css1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,43 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo">LOGO</div>
-            <div class="menu">
-            <ul>
-                <li>菜单1</li>
-                <li>菜单2</li>
-            </ul>
-            </div>
-        </div>
+        <Topnav/>
         <div class="banner">
             <h1>轱辘UI</h1>
             <h2>一个厉害的UI框架</h2>
             <p class="actions">
-                <a>GitHub</a>
-                <a>开始</a>
+                <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
 </template>
+<script>
+import Topnav from '../components/Topnav.vue'
+
+export default {
+   components:{Topnav}
+}
+</script>
+
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
+.banner {
+  padding: 100px 0;
   display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
