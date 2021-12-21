a28cfa2712de8468df8ac185c94c4ab3cc5fdf75
fendy <2457037852@qq.com>
2021-12-21 22:22:32 +0800

    实现aside切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo" @click="toggleMenu">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -8,8 +8,17 @@
   </div>
 </template>
 <script lang="ts">
+import { inject, Ref } from 'vue'
 export default {
   
+ setup(){
+    const menuVisible = inject<Ref<boolean>>('menuVisible') // get
+    const toggleMenu = ()=>{
+      menuVisible.value = !menuVisible.value
+    }
+    return {toggleMenu}
+  } 
+
 }
 </script>
 <style lang="scss" scoped>
@@ -17,6 +26,9 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
+  position:relative;
+  margin-top:0;
+  z-index:10;
   > .logo {
     max-width: 6em;
     margin-right: auto;
