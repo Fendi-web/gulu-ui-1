d793f126b0e5b3051b5bad1c9ca4d231460a453b
fendy <2457037852@qq.com>
2021-12-23 17:12:38 +0800

    背景渐变

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
     import { ComponentOptions } from 'vue'
     const component: ComponentOptions
-    export default component
+    export default ComponentOptions
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="topnavAndBanner">
         <Topnav/>
         <div class="banner">
             <h1>轱辘UI</h1>
@@ -11,7 +11,7 @@
         </div>
     </div>
 </template>
-<script>
+<script lang="ts">
 import Topnav from '../components/Topnav.vue'
 
 export default {
@@ -20,14 +20,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.topnavAndBanner {
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
-  > .actions{
+  
+  >.actions {
     padding: 8px 0;
     a {
       margin:0 8px;
