c2e0c172ccf0f2409de62d0d6e6048d87413612b
fendy <2457037852@qq.com>
2021-12-25 10:04:21 +0800

    显示源代码

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -49,9 +49,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.router-link-active {
-  text-decoration: underline;
-}
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -83,7 +81,7 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -92,10 +90,20 @@ aside {
 
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol {
     > li {
       padding: 4px;
+       >a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
+
     }
   }
 
