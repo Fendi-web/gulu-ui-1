4ce0a14153d64981675b5bb3525a59fb60d71e16
fendy <2457037852@qq.com>
2022-03-03 09:35:49 +0800

    完成

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./lib/gulu.scss";
 import "./index.scss";
-import { createApp } from 'vue'
-import App from './App.vue'
-import { router } from './router'
-import 'github-markdown-css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from './router';
+import 'github-markdown-css';
 import Markdown from './components/Markdown.vue';
 
 
